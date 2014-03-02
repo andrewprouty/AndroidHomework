3a556d4228002f8303d749a877e903388d36f246
andrewprouty <andyprouty@gmail.com>
2014-03-02 12:50:24 -0800

    Starting with Chapter 27 working Flickr model

---
+++ hw3-PhotoGallery/src/edu/prouty/hw3/photogallery/PhotoGalleryActivity.java
@@ -0,0 +1,10 @@
+package edu.prouty.hw3.photogallery;
+
+import android.support.v4.app.Fragment;
+
+public class PhotoGalleryActivity extends SingleFragmentActivity {
+    @Override
+    public Fragment createFragment() {
+        return new PhotoGalleryFragment();
+    }
+}
