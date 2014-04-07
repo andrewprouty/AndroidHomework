84caf8c80c9d0f58ceb23cc3bfa5e8d8a99cef3d
andrewprouty <andyprouty@gmail.com>
2014-04-06 19:25:11 -0700

    done

--- hw1-LifeCycle-Test/src/com/ap/LifeCycleMethods/test/LifeCycleActivityTest.java
+++ hw1-LifeCycle-Test/src/com/ap/LifeCycleMethods/test/LifeCycleActivityTest.java
@@ -43,8 +43,8 @@ ActivityInstrumentationTestCase2<LifeCycleActivity> {
 		countStart   = Integer.parseInt(mStartCount.getText().toString());
 		countResume  = Integer.parseInt(mResumeCount.getText().toString());
 		countPause   = Integer.parseInt(mPauseCount.getText().toString());
-		countStop   = Integer.parseInt(mStopCount.getText().toString());
-		countDestroy   = Integer.parseInt(mDestroyCount.getText().toString());
+		countStop    = Integer.parseInt(mStopCount.getText().toString());
+		countDestroy = Integer.parseInt(mDestroyCount.getText().toString());
 	}
 	private void initAsserts() {
 		Log.d(TAG, "initAsserts()");
@@ -54,18 +54,18 @@ ActivityInstrumentationTestCase2<LifeCycleActivity> {
 		assertStart   = Integer.parseInt(mStartCount.getText().toString());
 		assertResume  = Integer.parseInt(mResumeCount.getText().toString());
 		assertPause   = Integer.parseInt(mPauseCount.getText().toString());
-		assertStop   = Integer.parseInt(mStopCount.getText().toString());
-		assertDestroy   = Integer.parseInt(mDestroyCount.getText().toString());
+		assertStop    = Integer.parseInt(mStopCount.getText().toString());
+		assertDestroy = Integer.parseInt(mDestroyCount.getText().toString());
 	}
 	private void getActivityFields() { // Each time reset activity
 		mResetButton = (Button) mActivity.findViewById(com.ap.LifeCycleMethods.R.id.resetButton);
 
-		mCreateCount= (TextView) mActivity.findViewById(com.ap.LifeCycleMethods.R.id.onCreateCount);
+		mCreateCount=  (TextView) mActivity.findViewById(com.ap.LifeCycleMethods.R.id.onCreateCount);
 		mRestartCount= (TextView) mActivity.findViewById(com.ap.LifeCycleMethods.R.id.onRestartCount);
-		mStartCount= (TextView) mActivity.findViewById(com.ap.LifeCycleMethods.R.id.onStartCount);
-		mResumeCount= (TextView) mActivity.findViewById(com.ap.LifeCycleMethods.R.id.onResumeCount);
-		mPauseCount= (TextView) mActivity.findViewById(com.ap.LifeCycleMethods.R.id.onPauseCount);
-		mStopCount= (TextView) mActivity.findViewById(com.ap.LifeCycleMethods.R.id.onStopCount);
+		mStartCount=   (TextView) mActivity.findViewById(com.ap.LifeCycleMethods.R.id.onStartCount);
+		mResumeCount=  (TextView) mActivity.findViewById(com.ap.LifeCycleMethods.R.id.onResumeCount);
+		mPauseCount=   (TextView) mActivity.findViewById(com.ap.LifeCycleMethods.R.id.onPauseCount);
+		mStopCount=    (TextView) mActivity.findViewById(com.ap.LifeCycleMethods.R.id.onStopCount);
 		mDestroyCount= (TextView) mActivity.findViewById(com.ap.LifeCycleMethods.R.id.onDestroyCount);
 	}
 	// setUp and Tests
@@ -83,32 +83,23 @@ ActivityInstrumentationTestCase2<LifeCycleActivity> {
 		assertNotNull(mCreateCount);
 		setCounts();
 		// At launch assert values can be expected (not calculated)
-		Log.i(TAG, "---test1Launch_CreateStartResume()=1(Create, Start, Resume), same(others)");
+		Log.i(TAG, "---test1Launch_CreateStartResume()=1(Create, Start, Resume),=0(Pause,Restart), same(Stop/Destroy)");
 		initAsserts();
 		// expected, actual
 		assertEquals(1, assertCreate);
-		assertEquals(countPause, assertPause);
+		assertEquals(0, assertPause);
 		assertEquals(1, assertStart);
 		assertEquals(1, assertResume);
-		assertEquals(countRestart, assertRestart);
-		assertEquals(countStop, assertStop);
+		assertEquals(0, assertRestart);
+		assertEquals(countStop, assertStop); // zero if just installed
 		assertEquals(countDestroy, assertDestroy);
 	}
 	@UiThreadTest
-	// To be call-able switched to UIThreadTest so don't need Runnable() or waitForIdleSync()
 	public void test2PauseToResume() {
 		Log.i(TAG, "===test2PauseToResume()===");
 		setCounts();
 		mInstrumentation.callActivityOnPause(mActivity);
 		mInstrumentation.callActivityOnResume(mActivity);
-		/* mActivity.runOnUiThread(new Runnable() {
-			public void run() {
-				mInstrumentation.callActivityOnPause(mActivity);
-				mInstrumentation.callActivityOnResume(mActivity);
-				Log.d(TAG, "test2PauseToResume().run() mPauseCount: "+mPauseCount.getText());
-			}
-		});
-		mInstrumentation.waitForIdleSync(); */
 		initAsserts(); // set & log assertPause to current
 		Log.i(TAG, "---test2PauseToResume() assert=+1(Pause,Resume), same(others)");
 		// expected, actual
@@ -120,76 +111,41 @@ ActivityInstrumentationTestCase2<LifeCycleActivity> {
 		assertEquals(countStop, assertStop);
 		assertEquals(countDestroy, assertDestroy);
 	}
-	//@UiThreadTest
 	public void test3DestroyToResume() {
 		Log.i(TAG, "===test3DestroyResume()===");
 		setCounts();
-		//mInstrumentation.callActivityOnPause(mActivity);
-		//mActivity.finish();
+		mActivity.finish();
 		//Since we have turned on "Don't Keep Activities" this will kill the main activity
 		Activity foo = this.launchActivity("com.ap.LifeCycleMethods", FooActivity.class, null);
-		/*mActivity.runOnUiThread(new Runnable() {
-			public void run() {
-				Log.d(TAG, "test3().Runnable() ");
-			}
-		});*/
-		//mInstrumentation.waitForIdleSync();
-		Log.i(TAG, "test3DestroyResume() -2");
 		foo.finish();
 		//Now we can recreate the MainActivity 
-		Log.i(TAG, "test3DestroyResume() -1");
 		mInstrumentation = getInstrumentation();
-		Log.i(TAG, "test3DestroyResume() 0");
-		//mActivity = this.launchActivity("com.ap.LifeCycleMethods", LifeCycleActivity.class, null);
-		Log.i(TAG, "test3DestroyResume() 0.5");
-		mActivity = getActivity();
-		/*mActivity = this.getActivity();
-		Log.i(TAG, "test3DestroyResume() 0.5");
+		mActivity = this.getActivity();
 		mActivity.runOnUiThread(new Runnable() {
 			public void run() {
-				Log.i(TAG, "test3DestroyResume().run()");
-				mInstrumentation.callActivityOnCreate(mActivity, null);
-				Log.i(TAG, "test3DestroyResume().run() 1");
+				mInstrumentation.callActivityOnRestart(mActivity);
 				mInstrumentation.callActivityOnStart(mActivity);
-				Log.i(TAG, "test3DestroyResume().run() 2");
 				mInstrumentation.callActivityOnResume(mActivity);
-				Log.i(TAG, "test3DestroyResume().run() 3");
 			}
 		});
-		Log.i(TAG, "test3DestroyResume() 1");
-		mActivity = this.getActivity();
-		Log.i(TAG, "test3DestroyResume() 2");
-		mInstrumentation.callActivityOnCreate(mActivity, null);
-		Log.i(TAG, "test3DestroyResume() 3");
-		mInstrumentation.callActivityOnStart(mActivity);
-		Log.i(TAG, "test3DestroyResume() 4");
-		mInstrumentation.callActivityOnResume(mActivity);*/
-		Log.i(TAG, "test3DestroyResume() 5");
+		mInstrumentation.waitForIdleSync();
 		getActivityFields(); // new activity means new fields/re-fetch
-		Log.i(TAG, "test3DestroyResume() 6");
 		initAsserts();
-		Log.i(TAG, "---test3DestroyResume() assert +1(Pause,Create,Start,Resume), same(Restart)");
+		Log.i(TAG, "---test3DestroyResume() assert=+1(most), same(Create)");
+		// expected, actual
+		assertEquals(countCreate, assertCreate);
 		assertEquals(countPause+1, assertPause);
-		assertEquals(countCreate+1, assertCreate);
 		assertEquals(countStart+1, assertStart);
 		assertEquals(countResume+1, assertResume);
-		assertEquals(countRestart, assertRestart);
+		assertEquals(countRestart+1, assertRestart);
+		assertEquals(countStop+1, assertStop);
+		assertEquals(countDestroy+1, assertDestroy);
 	}
 	@UiThreadTest
-	public void xtest4Integration_ResetButton() {
-		Log.i(TAG, "===test4Integration_ResetButton()===");
-
-		Log.d(TAG, "---test4Integration() ONE: 1,0,1,1,0");
-		test1Launch_CreateStartResume();
-
-		Log.d(TAG, "---test4Integration() TWO: +(Pause,Resume), same(others)");
-		test2PauseToResume();
-
-		Log.d(TAG, "---test4Integration() 333: +1(Pause,Create,Start,Resume), same(Restart)");
-		test3DestroyToResume();
-
-		Log.i(TAG, "---test4Integration() 000: Reset should be zeros");
+	public void test4ResetButton() {
+		Log.i(TAG, "===test4ResetButton()===");
 		mResetButton.performClick();
+		Log.i(TAG, "---test4ResetButton() assert =0 (all)");
 		initAsserts(); // =(expected,actual)
 		assertEquals(0,assertCreate);
 		assertEquals(0,assertRestart);
